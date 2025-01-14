dd3d374f897294da416f94b1ca37ae4c3e325e3f
JaviUga <j.ugarrio.duro@gmail.com>
2025-01-14 15:59:56 +0100

    validarores y formulario reactivo

--- src/app/components/tiempo/tiempo.component.ts
+++ src/app/components/tiempo/tiempo.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-tiempo',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class TiempoComponent {
 
+  formulario !: FormGroup;
+
+  constructor(private formBuilder : FormBuilder) { 
+    this.iniciarFormulario();
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  /*
+   el metodo crea e inicia un formulario
+  */
+  iniciarFormulario(){
+    this.formulario = this.formBuilder.group({
+      ciudad: ['', [Validators.required , Validators.pattern('^[a-zA-Z ]*$')]],
+      codigoCiudad: ['', [Validators.required , Validators.pattern('^[0-9]*$')]]
+    });
+  }
+
+  consultar(){
+    console.log("formulario:" , this.formulario);
+    // Aquí se debería hacer la llamada a la API de tiempo
+    // y mostrar los resultados en pantalla.
+    // Este ejemplo solo imprime los valores del formulario en consola.
+  }
 }
